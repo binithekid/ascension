35017a3cfa0187bf92916e6366dc9487f2145f99
Bini Tek <bini.tek1@gmail.com>
2023-03-22 14:03:21 +0000

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,36 +29,17 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Ascension London</title>
+        <title>The Masonry Group</title>
         <meta name='description' content='Ascension London' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className='main'>
-        <Image
-          src={logo}
-          alt='Logo'
-          className='logo'
-          width={500}
-          height={500}
-        />
-        <h1 className='description'>
-          To attend our upcoming events enter your email below:
-        </h1>
-        <form className='formContainer' onSubmit={handleSubmit}>
-          <input
-            type='email'
-            className='input'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button className='submitButton' disabled={!email}>
-            Submit
-          </button>
-        </form>
-        {successMessage && (
-          <p className='successMessage'>Email submitted succesfully</p>
-        )}
+        <h1 className='logo'>TMG</h1>
+        <p className='subheading'>The Masonry Group</p>
+        <p className='description'>
+          We are currently undergoing maintnece, please check back soon
+        </p>
       </main>
     </>
   );
